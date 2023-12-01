d2410b0c1d13111c69f45f484a870a5fd3c4d386
Ali Sobhani <acdota@outlook.com>
2023-12-01 21:28:26 +0330

    Removed Unnecessery DIServices Class

--- TaskManager/Program.cs
+++ TaskManager/Program.cs
@@ -1,8 +1,6 @@
 using Domain.Models.Duties;
-using Infrastructure.Data.Servers;
 using Infrastructure.Repository;
 using Infrastructure.Repository.Contracts.EntityReposiories;
-using TaskManager.DependencyInjection;
 
 var builder = WebApplication.CreateBuilder(args);
 
